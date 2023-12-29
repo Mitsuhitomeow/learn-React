fe6a864569ca225d177a191db1f544bf863b7acd
Alexey Melnikov <cancem343@gmail.com>
2023-12-29 18:11:11 +0300

    refactor: update code for learn.

--- learn-react/src/components/main/index.jsx
+++ learn-react/src/components/main/index.jsx
@@ -1,24 +1,34 @@
-import { ways } from './../../data'
+import { useState } from 'react'
+import { ways, differences } from './../../data'
 import Button from './Button'
 import WayToTeach from './wayToTeach'
 
 export default function Main() {
+  let [contentType, setContentType] = useState(null)
+
+  function handleClick(text) {
+    setContentType(text)
+  }
+
   return (
     <main>
       <section>
         <h3>Наш подход к обучению</h3>
 
         <ul>
-          {ways.map(el => <WayToTeach title={el.title} description={el.description} />)}
+          {ways.map(el => <WayToTeach {...el} />)}
         </ul>
       </section>
 
       <section>
         <h3>Чем мы отличаемся от других</h3>
 
-        <Button>Подход</Button>
-        <Button>Доступность</Button>
-        <Button>Концентрация</Button>
+        <Button isActive={contentType === 'way'} onClick={() => handleClick('way')}>Подход</Button>
+        <Button isActive={contentType === 'easy'} onClick={() => handleClick('easy')}>Доступность</Button>
+        <Button isActive={contentType === 'program'} onClick={() => handleClick('program')}>Концентрация</Button>
+        <Button isActive={contentType === null} onClick={() => handleClick(null)}>Reset</Button>
+
+        { contentType ? <p>{differences[contentType]}</p> : <p>Нажми на кнопку.</p> }
       </section>
     </main>
   )
